6a0d53c1b3076c8e07f9e1ab1945126a6887a224
G10dero <155598731+G10dero@users.noreply.github.com>
2024-01-04 12:33:10 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,59 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int num1, num2, choice, result;
+
+int MainMenu(){
+	int choice;
     char decision = 'y';
-    do{
-    system("cls");
-    operation: cout<<"Enter first value: ";
+    cout<<"(1) Add \n(2) Subtract \n(3) Multiply \n(4) Divide \n";
+	cin>>choice;
+	system("cls");
+	return choice;
+}
+
+int add(int x, int y){
+	int sum = x + y;
+	return sum;
+}
+
+int subtract(int x, int y){
+	int diff = x - y;
+	return diff;
+}
+
+int multiply(int x, int y){
+	int prod = x * y;
+	return prod;
+}
+
+int divide(int x, int y){
+	int quotient = x / y;
+	return quotient;
+}
+	
+int main(){
+	int num1, num2, choice, result;
+    char decision = 'y';
+    cout<<"Enter first value: ";
     cin>>num1;
     cout<<"Enter second value: ";
     cin>>num2;
+    do{
+    	system("cls");
     	do{
-		    cout<<"(1) Add \n(2) Subtract \n(3) Multiply \n(4) Divide \n";
-		    cin>>choice;
-		    system("cls");
-			switch (choice){
+			switch (MainMenu()){
 				case 1 :
-					result = num1 + num2;
-		    		cout<<"The sum is: "<<result;
-		    		break;
-		    	
+					cout<<"The sum is: "<<add(num1, num2);
+					break;
 		    	case 2:
-		    		result = num1 - num2;
-		    		cout<<"The difference is: "<<result;
+		    		cout<<"The sum is: "<<subtract(num1, num2);
 					break;
 					
 				case 3:
-					result = num1 * num2;
-					cout<<"The product is: "<<result;
+					cout<<"The sum is: "<<multiply(num1, num2);
 					break;
 					
 				case 4:
-					result = num1 / num2;
-					cout<<"The quotient is: "<<result;
+					cout<<"The sum is: "<<divide(num1, num2);
 					break;
 					
 				default:
@@ -43,6 +65,7 @@ int main(){
 	cout<<"\n\nWould you like to try another operation?\n(Y) Yes\n(N) No\n";
 	cin>>decision;}
 	while (decision == 'y');
+	system("cls");
 	cout<<"Thank you!";
 	return 0;
 }
